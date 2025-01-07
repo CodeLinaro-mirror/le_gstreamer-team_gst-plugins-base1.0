fbfed4e393529f51cd86fae7b5e728716a65ed31
Marc Leeman <m.leeman@televic.com>
2025-01-07 09:13:20 +0100

    New upstream version 1.24.11

--- gst-libs/gst/video/gstvideodecoder.c
+++ gst-libs/gst/video/gstvideodecoder.c
@@ -1307,13 +1307,18 @@ caps_error:
   }
 }
 
+/* Must be called holding the GST_VIDEO_DECODER_STREAM_LOCK */
 static gboolean
-gst_video_decoder_handle_missing_data_default (GstVideoDecoder *
-    decoder, GstClockTime timestamp, GstClockTime duration)
+gst_video_decoder_handle_missing_data_default (GstVideoDecoder * decoder,
+    GstClockTime timestamp, GstClockTime duration)
 {
   GstVideoDecoderPrivate *priv;
 
   priv = decoder->priv;
+
+  /* Exit early in case the decoder has been resetted and hasn't received a new segment event yet. */
+  if (decoder->input_segment.format != GST_FORMAT_TIME)
+    return FALSE;
 
   if (priv->automatic_request_sync_points) {
     GstClockTime deadline =
@@ -2438,6 +2443,9 @@ gst_video_decoder_chain_forward (GstVideoDecoder * decoder,
     gst_video_decoder_add_buffer_info (decoder, buf);
 
   priv->input_offset += gst_buffer_get_size (buf);
+
+  if (GST_BUFFER_FLAG_IS_SET (buf, GST_BUFFER_FLAG_DECODE_ONLY))
+    GST_VIDEO_CODEC_FRAME_SET_DECODE_ONLY (priv->current_frame);
 
   if (priv->packetized) {
     GstVideoCodecFrame *frame;
