cf9235a3d0e626649a61dbd1b6968f9789605d18
Marc Leeman <marc.leeman@gmail.com>
2025-08-08 08:55:32 +0200

    New upstream version 1.26.5

--- ext/gl/gstgloverlay.c
+++ ext/gl/gstgloverlay.c
@@ -163,6 +163,7 @@ gst_gl_overlay_gl_stop (GstGLBaseFilter * base_filter)
   if (overlay->image_memory) {
     gst_memory_unref ((GstMemory *) overlay->image_memory);
     overlay->image_memory = NULL;
+    overlay->location_has_changed = TRUE;
   }
 
   if (overlay->vao) {
@@ -396,6 +397,7 @@ gst_gl_overlay_set_caps (GstGLFilter * filter, GstCaps * incaps,
 
   overlay->window_width = width;
   overlay->window_height = height;
+  overlay->geometry_change = TRUE;
 
   return TRUE;
 }
