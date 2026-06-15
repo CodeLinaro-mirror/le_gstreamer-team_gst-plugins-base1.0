5f5ba46f23cb9e6c4a875234575d8cc5313b2cf2
Marc Leeman <marc.leeman@gmail.com>
2026-06-15 08:45:38 +0200

    New upstream version 1.28.4

--- gst-libs/gst/tag/gstxmptag.c
+++ gst-libs/gst/tag/gstxmptag.c
@@ -1182,7 +1182,7 @@ gst_tag_list_from_xmp_buffer (GstBuffer * buffer)
 {
   GstTagList *list = NULL;
   GstMapInfo info;
-  gchar *xps, *xp1, *xp2, *xpe, *ns, *ne;
+  const gchar *xps, *xp1, *xp2, *xpe, *ns, *ne;
   gsize len, max_ft_len;
   gboolean in_tag;
   gchar *part = NULL, *pp;
@@ -1218,11 +1218,11 @@ gst_tag_list_from_xmp_buffer (GstBuffer * buffer)
   GST_LOG ("Starting xmp parsing");
 
   gst_buffer_map (buffer, &info, GST_MAP_READ);
-  xps = (gchar *) info.data;
+  xps = (const gchar *) info.data;
   len = info.size;
   g_return_val_if_fail (len > 0, NULL);
 
-  xpe = &xps[len + 1];
+  xpe = &xps[len];
 
   /* check header and footer */
   xp1 = g_strstr_len (xps, len, "<?xpacket begin");
@@ -1366,6 +1366,7 @@ gst_tag_list_from_xmp_buffer (GstBuffer * buffer)
               /* restore chars overwritten by '\0' */
               *d = '=';
               *ae = '"';
+              *pp = '\0';
             } else if (*ae == '\0' || *ae == ' ') {
               /* end of attr/value pair */
               as = &ae[1];
